e716364de594bc3bfcb358a2ab5b83f83a6664ad
Stefan Aichholzer <play@analogbird.com>
2014-09-04 13:49:23 +0200

    V2.1 - New API and extended features. (cherry picked from commit bc27aca)

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 /**
- * Load the popup, inject the content script and get the information we need to fly.
+ * Load the popup and get the information we need to fly from background.js.
  */
 window.addEventListener('load', function() {
 	chrome.extension.getBackgroundPage().EXT.currentTab(document);
